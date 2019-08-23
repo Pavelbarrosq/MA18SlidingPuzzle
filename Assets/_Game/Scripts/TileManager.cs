759831bdc36a089a021a4a6c73b729bc5f575093
Pavel Barros Quintanilla <pavelbarrosquintanilla@Pavels-MacBook-Pro.local>
2019-08-23 08:56:17 +0200

    First commit

---
+++ Assets/_Game/Scripts/TileManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+
+
+public class TileManager : MonoBehaviour
+{
+    public float moveSpeed = 0.2f;
+    public float shuffleSpeed = 0.1f;
+
+    private int rowSize;
+
+    public List<GameObject> tiles = new List<GameObject>();
+
+    public bool inputLock = false;
+
+    private void Start()
+    {
+        rowSize = (int)Mathf.Sqrt(tiles.Count);
+
+        StartCoroutine(Shuffle()); //kan bara ropa på metod på detta sätt om det är en Coroutine
+    }
+
+
+    public bool TilePressed(GameObject tile, bool shuffle)
+    {
+        if (inputLock)
+            return false;
+
+        GameObject emptyNeighbour = EmptyNeighbour(tile);
+
+        if (emptyNeighbour == null)
+            return false;
+        float speed = shuffle ? shuffleSpeed : moveSpeed; // lika som if sats ihopdraget
+
+        SwitchTiles(tile, emptyNeighbour, speed);
+        return true;
+
+        // Debug.Log("Tile:" + tile.name);
+    }
+
+
+    private IEnumerator Shuffle()
+    {
+        for (int i = 0; i < 50;)
+        {
+            if (TilePressed(tiles[Random.Range(0, tiles.Count)], true))
+            {
+                i++;
+                yield return new WaitForSeconds(shuffleSpeed * 1.1f);
+            }
+                
+        }
+    }
+
+
+    private void SwitchTiles(GameObject tile1, GameObject tile2, float speed)
+    {
+
+        inputLock = true;
+        Vector3 pos = tile1.transform.position;
+
+        LeanTween.move(tile1, tile2.transform.position, speed).setEaseInCubic()
+            .setOnComplete(() => { inputLock = false; });
+        LeanTween.move(tile2, pos, speed);
+
+        //tile1.transform.position = tile2.transform.position;
+
+        //tile2.transform.position = pos;
+
+        int index1 = TileIndex(tile1);
+        int index2 = TileIndex(tile2);
+
+        tiles[index1] = tile2;
+        tiles[index2] = tile1;
+
+
+
+    }
+
+
+    private int TileIndex(GameObject tile)
+    {
+        return tiles.IndexOf(tile);
+    }
+
+
+    GameObject EmptyNeighbour (GameObject tile)
+    {
+        foreach (GameObject gameObject in Neighbours(tile))
+        {
+            if (gameObject.tag == "Empty")
+                return gameObject;
+        }
+
+        return null;
+    }
+
+
+    private List<GameObject> Neighbours(GameObject tile)
+    {
+        int index = TileIndex(tile);
+        List<GameObject> neighbours = new List<GameObject>();
+
+        // Right
+        int right = index + 1;
+        if (InRange(right) && (index + 1) % rowSize != 0)
+            neighbours.Add(tiles[right]);
+
+        //Left
+        int left = index - 1;
+        if (InRange(left) && index % rowSize != 0)
+            neighbours.Add(tiles[left]);
+
+        //Over
+        int over = index - rowSize;
+        if (InRange(over))
+            neighbours.Add(tiles[over]);
+
+        //Under
+        int under = index + rowSize;
+        if (InRange(under))
+            neighbours.Add(tiles[under]);
+
+
+
+        return neighbours;
+    }
+
+    private bool InRange(int index)
+    {
+        return (index >= 0 && index < tiles.Count);
+    }
+}
